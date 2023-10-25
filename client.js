8c657097d1179a825d4e030cc37c361f0fcec744
tibirabotto <tibira.botto@gmail.com>
2023-10-25 11:13:29 -0400

    constants

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const net = require("net");
-
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "127.0.0.1",
-    port: 3000,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
