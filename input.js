3c40c2a6eb22271639341028d26abd6f948ce25b
tibirabotto <tibira.botto@gmail.com>
2023-10-25 11:10:24 -0400

    input

---
+++ input.js
@@ -0,0 +1,7 @@
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+};
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 3000,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("name", (name) => {
-    console.log("Server says: ", name);
-  });
-
-  conn.on("connect", () => {
-    conn.write("Hello from client!");
-  });
-
-
-  return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
